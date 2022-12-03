a25ae92aca10225928c9c68a9db1e208758f5d67
Paul Bowsher <paul.bowsher@gmail.com>
2022-12-03 22:37:16 +0000

    SPACES

--- day03/main.go
+++ day03/main.go
@@ -9,24 +9,24 @@ import (
 )
 
 func main() {
-	// Open the input.txt file
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+  // Open the input.txt file
+  file, err := os.Open("input.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
 
-	// Create a scanner to read the file line by line
-	scanner := bufio.NewScanner(file)
+  // Create a scanner to read the file line by line
+  scanner := bufio.NewScanner(file)
 
-	// Create a slice to store the lines
-	var rucksacks []string
+  // Create a slice to store the lines
+  var rucksacks []string
 
-	// Loop over the lines in the file
-	for scanner.Scan() {
-		// Append the line to the slice
-		rucksacks = append(rucksacks, scanner.Text())
-	}
+  // Loop over the lines in the file
+  for scanner.Scan() {
+    // Append the line to the slice
+    rucksacks = append(rucksacks, scanner.Text())
+  }
 
   // Map to count the number of times each character appears in both compartments of each rucksack
   charCount := make(map[rune]int)
@@ -36,13 +36,13 @@ func main() {
     first := rucksack[:len(rucksack)/2]
     second := rucksack[len(rucksack)/2:]
 
-		counted := make(map[rune]bool)
+    counted := make(map[rune]bool)
 
     // Go through each character in the first compartment and check if it appears in the second compartment
     for _, char := range first {
       if !counted[char] && strings.ContainsRune(second, char) {
         charCount[char]++
-				counted[char] = true
+        counted[char] = true
       }
     }
   }
@@ -52,13 +52,13 @@ func main() {
 
   // Go through each character and its count in the map
   for char, count := range charCount {
-		// Lowercase characters have priorities 1 through 26
-		if char >= 'a' && char <= 'z' {
-			sum += count * (int(char) - 'a' + 1)
-		} else {
-			// Uppercase characters have priorities 27 through 52
-			sum += count * (int(char) - 'A' + 27)
-		}
+    // Lowercase characters have priorities 1 through 26
+    if char >= 'a' && char <= 'z' {
+      sum += count * (int(char) - 'a' + 1)
+    } else {
+      // Uppercase characters have priorities 27 through 52
+      sum += count * (int(char) - 'A' + 27)
+    }
   }
 
   fmt.Println(sum)
